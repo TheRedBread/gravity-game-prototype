784056f3d97220b01404678452ed0e6f31fb9399
TheRedBread <159266466+TheRedBread@users.noreply.github.com>
2025-04-03 17:33:05 +0200

    elevator and elevator shaft

--- global elements/game_manager.gd
+++ global elements/game_manager.gd
@@ -6,8 +6,9 @@ const BASIC_POST_PROCESS = preload("res://global elements/basic_post_process.tsc
 
 
 func _ready():
-	
+	pass
 	add_child(BASIC_POST_PROCESS.instantiate())
+
 
 func close_game():
 	GameSaveSystem.save_game()
