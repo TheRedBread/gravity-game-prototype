8b46bf5e272227ac61c785d370b1e9847478dd9d
TheRedBread <159266466+TheRedBread@users.noreply.github.com>
2025-04-25 20:58:01 +0200

    lever for hydraulic press

--- levels/power/hydraulic press/hydraulic_press.gd
+++ levels/power/hydraulic press/hydraulic_press.gd
@@ -6,16 +6,24 @@ extends Node2D
 @export var press_speed : float = 1.0
 @export var animation_offset : float = 0.0
 
+var was_on : bool 
+
 func _ready() -> void:
+	start()
+
+func start():
 	animation_player.speed_scale = press_speed
 	await get_tree().create_timer(animation_offset).timeout
 	animation_player.play("crash")
 
 func _physics_process(delta: float) -> void:
-	
-	if power_supply and power_supply.on:
+	if !power_supply:
 		return
-
+	
+	if power_supply and !power_supply.on and was_on:
+		
+		start()
+	was_on = power_supply.on
 
 func crash_finished():
 	if power_supply and power_supply.on:
